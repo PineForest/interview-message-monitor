656c8db350679a71921f81b4987b845be87cd358
PineForest <davidkwilliams@yahoo.com>
2016-03-29 20:27:40 -0700

    Added LGPL.

--- src/main/java/interview/question/MessageMonitor.java
+++ src/main/java/interview/question/MessageMonitor.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2016  David Williams
+ *
+ * This file is part of the interview-message-monitor project.
+ *
+ * interview-message-monitor is free software: you can redistribute it and/or modify it under the terms of the
+ * Lesser GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * interview-message-monitor is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License along with interview-message-monitor.
+ * If not, see <a href="http://www.gnu.org/licenses/">www.gnu.org/licenses/</a>.
+ */
+
 package interview.question;
 
 /**
